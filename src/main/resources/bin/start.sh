d02e0d5aa9433c8397d55ef20c9f265ad56797d4
tankbing <tankdev@163.com>
2018-08-10 14:36:14 +0800

    log to null

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -4,4 +4,4 @@ then
     ENV="dev";
 fi
 
-java -Xms256m -Xmx512m -server -Dspring.config.location=../config/application.properties,../config/messages_en.properties,../config/env/%ENV%/env-config.properties -jar ../lib/packaging-demo.jar
+nohup java -Xms256m -Xmx512m -server -Dspring.config.location=../config/application.properties,../config/messages_en.properties,../config/env/$ENV/env-config.properties -jar ../lib/packaging-demo.jar >/dev/null 2>&1 &
